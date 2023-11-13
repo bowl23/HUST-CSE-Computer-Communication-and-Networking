4b48a0ee02a588ae404d24a8eba03dac20532fbe
bowl23 <1467059904@qq.com>
2023-11-13 20:42:55 +0800

    renew

--- main.cpp
+++ main.cpp
@@ -6,17 +6,19 @@
 #pragma comment (lib,"wws2_32.lib")
 using namespace std;
 
-int getUdpSocket() {
+int getUdpSocket() {//生成并初始化一个udpsocket
 	WORD ver = MAKEWORD(2, 2);
 	WSADATA lpDATA;
 	int err = WSAStartup(ver, &lpDATA);
-	if (err != 0) return -1;
+	if (err != 0) 
+		return -1;
 	int udpsocket = socket(AF_INET, SOCK_DGRAM, 0);
-	if (udpsocket == INVALID_SOCKET) return -2;
+	if (udpsocket == INVALID_SOCKET) 
+		return -2;
 	return udpsocket;
 }
 
-sockaddr_in getAddr(const char* ip, int port) {
+sockaddr_in getAddr(const char* ip, int port) {//根据输入的IP地址和端口号构造出一个存储着地址的sockaddr_in类型
 	sockaddr_in addr;
 	addr.sin_family = AF_INET;
 	addr.sin_port = htons(port);
@@ -25,7 +27,7 @@ sockaddr_in getAddr(const char* ip, int port) {
 }
 
 
-char* RequestDownloadPack(char* content, int& datalen, int type) {
+char* RequestDownloadPack(char* content, int& datalen, int type) {//构造RPQ数据包，下载文件
 	int len = strlen(content);
 	char* buf = new char[len + 2 + 2 + type];
 	buf[0] = 0x00;
@@ -33,15 +35,15 @@ char* RequestDownloadPack(char* content, int& datalen, int type) {
 	memcpy(buf + 2, content, len);
 	memcpy(buf + 2 + len, "\0", 1);
 	if (type == 5)
-		memcpy(buf + 2 + len + 1, "octet", 5);
+		memcpy(buf + 2 + len + 1, "octet", 5);//二进制文件
 	else
-		memcpy(buf + 2 + len + 1, "netascii", 8);
+		memcpy(buf + 2 + len + 1, "netascii", 8);//文本文件
 	memcpy(buf + 2 + len + 1 + type, "\0", 1);
 	datalen = len + 2 + 1 + type + 1;
 	return buf;
 }
 
-char* RequestUploadPack(char* content, int& datalen, int type) {
+char* RequestUploadPack(char* content, int& datalen, int type) {//构造WRQ数据包，上传文件
 	int len = strlen(content);
 	char* buf = new char[len + 2 + 2 + type];
 	buf[0] = 0x00;
@@ -49,26 +51,26 @@ char* RequestUploadPack(char* content, int& datalen, int type) {
 	memcpy(buf + 2, content, len);
 	memcpy(buf + 2 + len, "\0", 1);
 	if (type == 5)
-		memcpy(buf + 2 + len + 1, "octet", 5);
+		memcpy(buf + 2 + len + 1, "octet", 5);//二进制文件
 	else
-		memcpy(buf + 2 + len + 1, "netascii", 8);
+		memcpy(buf + 2 + len + 1, "netascii", 8);//文本文件
 	memcpy(buf + 2 + len + 1 + type, "\0", 1);
 	datalen = len + 2 + 1 + type + 1;
 	return buf;
 }
 
-char* AckPack(short& no) {
+char* AckPack(short& no) {//构造ACK数据包
 	char* ack = new char[4];
 	ack[0] = 0x00;
 	ack[1] = 0x04;
-	no = htons(no);
+	no = htons(no);//将主机字节序转换成网络字节序
 	memcpy(ack + 2, &no, 2);
 	no = ntohs(no);
 	return ack;
 }
 
 
-char* MakeData(short& no, FILE* f, int& datalen) {
+char* MakeData(short& no, FILE* f, int& datalen) {//构造DATA数据包
 	char temp[512];
 	int sum = fread(temp, 1, 512, f);
 	if (!ferror(f)) {
@@ -83,10 +85,11 @@ char* MakeData(short& no, FILE* f, int& datalen) {
 		return buf;
 
 	}
-	return NULL;
+	else
+		return NULL;
 }
 
-void print_time(FILE* fp) {
+void print_time(FILE* fp) {//日志
 	time_t t;
 	time(&t);
 	char stime[100];
@@ -100,12 +103,12 @@ void print_time(FILE* fp) {
 
 
 int main() {
-	FILE* fp = fopen("TFTP_client.log", "a");
+	FILE* fp = fopen("TFTP_client.log", "a");//打开日志
 	char commonbuf[2048];
 	int buflen;
 	int Numbertokill;
 	int Killtime;
-	clock_t start, end;
+	clock_t start, end;//分别记录开始和结束的时间，用于计算传输速率 
 	double runtime;
 	SOCKET sock = getUdpSocket();
 	sockaddr_in addr;
@@ -114,38 +117,207 @@ int main() {
 	setsockopt(sock, SOL_SOCKET, SO_RCVTIMEO, (char*)&recvTimeout, sizeof(int));
 	setsockopt(sock, SOL_SOCKET, SO_RCVTIMEO, (char*)&sendTimeout, sizeof(int));
 	while (1) {
-		printf("#######1.上传文件######");
-		printf("#######2.下载文件######");
-		printf("#######0.关闭TFTP客户端######");
+		printf("#######		1.上传文件			######");
+		printf("#######		2.下载文件			######");
+		printf("#######		0.关闭TFTP客户端		######");
 		int choice;
 		scanf("%d", &choice);
-		if (choice == 1) {
-			addr = getAddr("127. 0 .0 .1 ", 69);
-			printf(" 请输入要上传文件的全名： \n");
+		//选择功能
+		if (choice == 1) {//上传文件
+			addr = getAddr("127. 0 .0 .1 ", 69);//第一个数据包总是发向本机的69端口
+
+			printf(" 请输入要上传文件的全名： \n");//用户输入文件的名称，储存在变量name中
+			char name[1000];
+
+			scanf("%s", name);
+
+			int type;
+			printf(" 请选择上传文件的方式： 1. netascii 2.octet\n");//用户选择文件的格式
+			scanf("%d%", &type);
+
+			if (type == 1)//文本文件
+				type = 8;
+			else
+				type = 5;//二进制文件
+
+			int datalen;
+			char* sendData = RequestUploadPack(name, datalen, type);
+			buflen = datalen;
+			Numbertokill = 1;//表示数据包超时的次数
+			memcpy(commonbuf, sendData, datalen);
+
+			int res = sendto(sock, sendData, datalen, 0, (sockaddr*)&addr, sizeof(addr));//第一次开始发送WRQ包
+			start = clock();//开始计时
+			print_time(fp);
+			fprintf(fp, "send WRQ for file:% s\n", name);
+			Killtime = 1;//表示sendto的超时次数，与recv_from超市分开计算
+
+
+
+			while (res != datalen) {//如果sendto函数失败，则立即重新sendto
+
+				std::cout << "send WRQ failed:" << Killtime << "times" << std::endl;
+				//规定sendto失败十次就自动结束传输
+				if (Killtime <= 10) {
+					res = sendto(sock, commonbuf, buflen, 0, (sockaddr*)&addr, sizeof(addr));
+					Killtime++;
+				}
+				else
+					break;
+			}
+
+
+			if (Killtime > 10)
+				continue;
+
+			delete[]sendData;
+			FILE* f = fopen(name, "rb");
+
+			if (f == NULL) {
+				std::cout << "Fi1e" << name << "open failed!" << std::endl;
+				continue;
+			}
+
+			short block = 0;
+			datalen = 0;
+			int RST = 0;
+			int Fullsize = 0;
+
+			while (1) {
+				char buf[1024];
+				sockaddr_in server;//从反馈的数据包中获得其分配的端口号信息
+
+				int len = sizeof(server);
+				res = recvfrom(sock, buf, 1024, 0, (sockaddr*)&server, &len);
+
+				if (res == -1) {//未收到数据
+
+					printf("%d ", Numbertokill);
+					if (Numbertokill > 10) {//连续十次未收到响应
+						printf("No acks get. transmission failed！\n");
+						print_time(fp);
+						fprintf(fp, "Upload file: %s failed. \n", name);//打印错误提示并结束传输
+						break;
+					}
+
+					int res = sendto(sock, commonbuf, buflen, 0, (sockaddr*)&addr, sizeof(addr));//重传上一个数据包
+					RST++;
+					std::cout << "resend last blk" << std::endl;
+
+					Killtime = 1;//同上处理sendto超时的情况
+					while (res != buflen) {
+						std::cout << "resend last blk failed:" << Killtime << "times" << std::endl;
+						if (Killtime <= 10) {
+							res = sendto(sock, commonbuf, buflen, 0, (sockaddr*)&addr, sizeof(addr));
+							Killtime++;
+						}
+						else
+							break;
+					}
+					if (Killtime > 10)
+						break;
+					Numbertokill++;
+				}
+
+				if (res > 0) {
+					short flag;
+					memcpy(&flag, buf, 2);
+					flag = ntohs(flag);
+
+					if (flag == 4) {//收到的是ACK数据包
+
+						short no;
+						memcpy(&no, buf + 2, 2);
+						no = ntohs(no);
+						if (no == block) {
+							addr = server;
+							if (feof(f) && datalen != 516) {//最后一个包的字节长度不为516，则说明文件已全部上传完毕
+								std::cout << "upload finished !" << std::endl;//结束传输
+								end = clock();
+
+								runtime = (double)(end - start) / CLOCKS_PER_SEC;//计算传输时间
+								print_time(fp);
+								printf("Average transmission rate: ％.21f kb/s \n", Fullsize / runtime / 1000);
+								fprintf(fp, "Upload file: %s finished.resent times: %d;Fu11size: %d\n", name, RST, Fullsize);
+								break;
+							}
+
+							block++;//没上传完，则制作下一个DATA包
+							sendData = MakeData(block, f, datalen);
+							buflen = datalen;
+							Fullsize += datalen - 4;//fullsize要去除数据包中头部的长度
+							Numbertokill = 1;//重置当前数据包的重发次数
+							memcpy(commonbuf, sendData, datalen);//更新commonbuf中的内容，准备下一次可能的重传
+
+							if (sendData == NULL) {//如果在构建数据包的过程中失败了
+								std::cout << "Fi1e reading mistakes!" << std::endl;
+								break;
+							}//将刚刚构造的DATA报发送出去
+							int res = sendto(sock, sendData, datalen, 0, (sockaddr*)&addr, sizeof(addr));
+							Killtime = 1;
+							while (res != datalen) {
+								std::cout << "send block" << block << "failed :" << Killtime << "times" << std::endl;
+								if (Killtime <= 10) {
+									res = sendto(sock, commonbuf, buflen, 0, (sockaddr*)&addr, sizeof(addr));
+									Killtime++;
+								}
+								else
+									break;
+							}
+
+							if (Killtime > 10)
+								continue;
+							std::cout << "Pack No= " << block << std::endl;//向用户输出当前正在传输的数据包编号
+
+						}
+					}
+
+
+
+					if (flag == 5) {//处理ERROR包
+						short errorcode;
+						memcpy(&errorcode, buf + 2, 2);//拆解ERROR获得错误码
+						errorcode = ntohs(errorcode);
+						char strError[1024];
+						int iter = 0;
+						while (*(buf + iter + 4) != 0) {
+							memcpy(strError + iter, buf + iter + 4, 1);
+							++iter;
+						}
+						*(strError + iter + 1) = '\0';
+						std::cout << "Error" << errorcode << "" << strError << std::endl;
+						print_time(fp);
+						fprintf(fp, "Error %d %s\n", errorcode, strError);
+						break;
+					}
+				}
+			}
+			fclose(f);
+		}
+		if (choice == 2) {
+			addr = getAddr("127.0.0.1", 69);
+			printf("请输入要下载文件的全名：\n");
 			char name[1000];
 			int type;
 			scanf("%s", name);
-			printf(" 请选择上传文件的方式： 1. netascii 2.octet\n");
-			scanf("%d%", &type);
+			printf("请选择下载文件的方式：1.netascii 2.octet\n");
+			scanf("%d", &type);
 			if (type == 1)
 				type = 8;
 			else
 				type = 5;
 			int datalen;
-			char* sendData = RequestUploadPack(name, datalen, type);
+			char* sendData = RequestDownloadPack(name, datalen, type);
 			buflen = datalen;
 			Numbertokill = 1;
 			memcpy(commonbuf, sendData, datalen);
 			int res = sendto(sock, sendData, datalen, 0, (sockaddr*)&addr, sizeof(addr));
 			start = clock();
 			print_time(fp);
-			fprintf(fp, "send WRQ for file:% s\n", name);
 			Killtime = 1;
-
-
-
-			while (res != datalen) {
-				std::cout << "send WRQ failed:" << Killtime << "times" << std::endl;
+			while (res !=datalen)
+			{
+				std:: cout<< "send RRQ failed:" << Killtime << "times" << std::endl;
 				if (Killtime <= 10) {
 					res = sendto(sock, commonbuf, buflen, 0, (sockaddr*)&addr, sizeof(addr));
 					Killtime++;
@@ -155,128 +327,113 @@ int main() {
 			}
 			if (Killtime > 10)
 				continue;
-			delete[] sendData;
-			FILE* f = fopen(name, "rb");
-
+			delete[]sendData;
+			FILE* f = fopen(name, "wb");
 			if (f == NULL) {
-				std::cout << "Fi1e" << name << "open failed!" << std::endl;
+				std::cout << "File" << name << "open failed" << std::endl;
 				continue;
 			}
-			short block = 0;
-			datalen = 0;
+			int want_recv = 1;
 			int RST = 0;
 			int Fullsize = 0;
 			while (1) {
 				char buf[1024];
 				sockaddr_in server;
-
 				int len = sizeof(server);
 				res = recvfrom(sock, buf, 1024, 0, (sockaddr*)&server, &len);
-				if (res == -1) {
-					printf("%d ", Numbertokill);
+				if(res==-1)
 					if (Numbertokill > 10) {
-
-						printf("No acks get.transmission failed\n");
+						printf("No block get.transmission failed\n");
 						print_time(fp);
-						fprintf(fp, "Upload file: %s failed. \n", name);
+						fprintf(fp, "Download file: %s failed.\n", name);
 						break;
 					}
-					int res = sendto(sock, commonbuf, buflen, 0, (sockaddr*)&addr, sizeof(addr));
-					RST++;
-					std::cout << "resend last blk" << std::endl;
+				int res = sendto(sock, commonbuf, buflen, 0, (sockaddr*)&addr, sizeof(addr));
+				RST++;
+				std::cout << "resend last blk" << std::endl;
+				Killtime = 1;
+				while (res!=buflen)
+				{
+					std::cout << "resend last blk failed:" << Killtime << "time" << std::endl;
+					if (Killtime <= 10) {
+						res = sendto(sock, commonbuf, buflen, 0, (sockaddr*)&addr, sizeof(addr));
+						Killtime++;
+					}
+					else
+						break;
+				}
+				if (Killtime > 10)
+					break;
+				Numbertokill++;
+			}
+			if (res > 0) {
+				short flag;
+				memcpy(&flag, buf, 2);
+				flag = ntohs(flag);
+				if (flag == 3) {
+					addr = server;
+					short no;
+					memcpy(&no, buf + 2, 2);
+					no = ntohs(no);
+					std::cout << "Pack No=" << no << std::endl;
+					char* ack = AckPack(no);
+					int sendlen = sendto(sock, ack, 4, 0, (sockaddr*)&addr, sizeof(addr));
 					Killtime = 1;
-					while (res != buflen) {
-						std::cout << "resend last blk failed:" << Killtime << "times" << std::endl;
+					while (sendlen!=4)
+					{
+						std::cout << "resend last ack failed:" << Killtime << "times" << std::endl;
 						if (Killtime <= 10) {
-							res = sendto(sock, commonbuf, buflen, 0, (sockaddr*)&addr, sizeof(addr));
+							sendlen = sendto(sock, ack, 4, 0, (sockaddr*)&addr, sizeof(addr));
 							Killtime++;
 						}
 						else
 							break;
-						if (Killtime > 10)
+					}
+					if (Killtime > 10)
+						break;
+					if (no == want_recv) {
+						buflen = 4;
+						Numbertokill = 1;
+						memcpy(commonbuf, ack, 4);
+						fwrite(buf + 4, res - 4, 1, f);
+						Fullsize += res - 4;
+						if (res - 4 >= 0 && res - 4 < 512) {
+							std::cout << "download finished" << std::endl;
+							end = clock();
+							runtime = (double)(end - start) / CLOCKS_PER_SEC;
+							print_time(fp);
+							printf("Average transmission rate: &.2f kb/s\n", Fullsize / runtime / 1000);
+							fprintf(fp, "Download file:%s finished.resent times:%d;Fullsize:%d\n", name, RST, Fullsize);
 							break;
-						Numbertokill++;
-					}
-					if (res > 0) {
-						short flag;
-						memcpy(&flag, buf, 2);
-						flag = ntohs(flag);
-						if (flag == 4) {
-
-							short no;
-							memcpy(&no, buf + 2, 2);
-							no = ntohs(no);
-							if (no == block) {
-								addr = server;
-
-								if (feof(f) && datalen != 516) {
-
-									std::cout << "upload finished !" << std::endl;
-									end = clock();
-
-									runtime = (double)(end - start) / CLOCKS_PER_SEC;
-									print_time(fp);
-									printf("Average transmission rate: ％.21f kb/s \n", Fullsize / runtime / 1000);
-									fprintf(fp, "Upload file: %s finished.resent times: %d;Fu11size: %d\n", name, RST, Fullsize);
-									break;
-								}
-								block++;
-								sendData = MakeData(block, f, datalen);
-								buflen = datalen;
-								Fullsize += datalen - 4;
-								Numbertokill = 1;
-								memcpy(commonbuf, sendData, datalen);
-								if (sendData == NULL) {
-									std::cout << "Fi1e read ing mistakes!" << std::endl;
-									break;
-								}
-								int res = sendto(sock, sendData, datalen, 0, (sockaddr*)&addr, sizeof(addr));
-								Killtime = 1;
-								while (res != datalen) {
-									std::cout << "send block" << block << "failed :" << Killtime << "times" << std::endl;
-									if (Killtime <= 10) {
-										res = sendto(sock, commonbuf, buflen, 0, (sockaddr*)&addr, sizeof(addr));
-										Killtime++;
-									}
-									else
-										break;
-								}
-								if (Killtime > 10)
-									continue;
-								std::cout << "Pack № = " << block << std::endl;
-							}
 						}
-						if (flag == 5) {
-							short errorcode;
-							memcpy(&errorcode, buf + 2, 2);
-							errorcode = ntohs(errorcode);
-							char strError[1024];
-							int iter = 0;
-							while (*(buf + iter + 4) != 0) {
-								memcpy(strError + iter, buf + iter + 4, 1);
-							}
-
-						}
-
-
-
+						want_recv++;
 					}
-
-
-
-
-
-
 				}
-
-
+				if (flag == 5) {
+					short errorcode;
+					memcpy(&errorcode, buf + 2, 2);
+					errorcode = ntohs(errorcode);
+					char strError[1024];
+					int iter = 0;
+					while (*(buf+iter+4)!=0)
+					{
+						memcpy(strError + iter, buf + iter + 4, 1);
+						++iter;
+					}
+					*(strError + iter + 1) = '\0';
+					std::cout << "Error" << errorcode << " " << strError << std::endl;
+					print_time(fp);
+					fprintf(fp, "Error %d %s\n", errorcode, strError);
+					break;
+				}
 			}
 		}
-
-
-
-
-
-
+		fclose(f);
 	}
+	if(choice==0)
+		break;
+}
+fclose(fp);
+return0;
+}
 
